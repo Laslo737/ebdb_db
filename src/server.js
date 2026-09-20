989952a03ea1b0a42ac069510fa8ac00e806f70e
Nikita <powar810321@gmail.com>
2026-09-21 03:13:30 +0400

    port change

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
-const { createApp } = require('./app');
-const { env } = require('./config/env');
+const { createApp } = require("./app");
+const { env } = require("./config/env");
 
 const app = createApp();
 
-app.listen(env.port, () => {
+app.listen(env.port, "127.0.0.1", () => {
   console.log(`Server started on port ${env.port}`);
 });
